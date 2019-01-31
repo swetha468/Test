8e48a2387370316bf08fda2a4d9eaaf9b86ac92a
dhaswetha <dhaswetha@deloitte.com>
2019-01-31 13:43:17 +0530

    area code

---
+++ TestArea.py
@@ -0,0 +1,9 @@
+import functions
+
+sqaure_area=functions.area_of_a_square(2)
+
+print(sqaure_area)
+
+triangle_area=functions.area_of_triangle(2,4)
+
+print(triangle_area)
---
+++ functions.py
@@ -0,0 +1,5 @@
+def area_of_a_square(a):
+    return a*a
+
+def area_of_triangle(b,h):
+    return 1/2*(b*h)
